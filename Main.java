d245b88c2e6c7c7d259e0e0c0469d3f8aa3d5681
luissao12 <luisitoalomia02@gmail.com>
2025-02-23 15:10:01 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio6;
+package ejercicio7;
 
 /**
  *
@@ -10,10 +10,12 @@ package ejercicio6;
  */
 public class Main {
 
-    
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-      Nota opcion = new Nota();
-      opcion.menu();
+        Temperatura opcion = new Temperatura();
+        opcion.menu();
     }
     
 }
